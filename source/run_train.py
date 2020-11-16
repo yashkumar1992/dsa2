dc529a793c1b95031dfa61e32857055212496d08
arita37 <noelkev0@gmail.com>
2020-11-16 22:26:37 +0900

    clean into one single config file

--- source/run_train.py
+++ source/run_train.py
@@ -3,9 +3,9 @@
 """
 cd analysis
 
-python source/run_train.py  run_train --model_name elasticnet  --path_data data/input/train/    --path_output data/output/a01_elasticnet/
+python source/run_train.py  run_train --config_model_name elasticnet  --path_data data/input/train/    --path_output data/output/a01_elasticnet/
 
-! activate py36 && python source/run_train.py  run_train   --n_sample 100  --model_name lightgbm  --path_model_config source/config_model.py  --path_output /data/output/a01_test/     --path_data /data/input/train/    
+! activate py36 && python source/run_train.py  run_train   --n_sample 100  --config_model_name lightgbm  --path_model_config source/config_model.py  --path_output /data/output/a01_test/     --path_data /data/input/train/
 
 
 
@@ -60,8 +60,8 @@ def save_list(path, name_list, glob):
     import pickle, os
     os.makedirs(path, exist_ok=True)
     for t in name_list:
-        log(t)
-        pickle.dump(glob[t], open(f'{t}', mode='wb'))
+        log(t, f'{path}/{t}.pkl')
+        pickle.dump(glob[t], open(f'{path}/{t}.pkl', mode='wb'))
 
 
 def save(obj, path):
@@ -88,7 +88,7 @@ def load_function_uri(uri_name="path_norm"):
     
     import importlib, sys
     from pathlib import Path
-    pkg = uri_name.split(":")
+    pkg = uri_name.split("::")
 
     assert len(pkg) > 1, "  Missing :   in  uri_name module_name:function_or_class "
     package, name = pkg[0], pkg[1]
@@ -107,7 +107,7 @@ def load_function_uri(uri_name="path_norm"):
             #### import Absolute Path model_tf.1_lstm
             model_name   = Path(package).stem  # remove .py
             package_name = str(Path(package).parts[-2]) + "." + str(model_name)
-            #log(package_name, model_name)
+            #log(package_name, config_model_name)
             return  getattr(importlib.import_module(package_name), name)
 
         except Exception as e2:
@@ -306,7 +306,7 @@ def train(model_dict, dfX, cols_family, post_process_fun):
     """
     model_pars, compute_pars = model_dict['model_pars'], model_dict['compute_pars']
     data_pars = model_dict['data_pars']
-    model_name, model_path = model_pars['model_name'], model_pars['model_path']
+    model_name, model_path = model_pars['config_model_name'], model_pars['model_path']
     metric_list = compute_pars['metric_list']
 
     log("#### Data preparation #############################################################")
@@ -364,8 +364,8 @@ def train(model_dict, dfX, cols_family, post_process_fun):
 
 ####################################################################################################
 ############CLI Command ############################################################################
-def run_train(model_name, path_data, path_output, path_config_model="source/config_model.py", n_sample=5000,
-              run_preprocess=1,):
+def run_train(config_model_name, path_data, path_output, path_config_model="source/config_model.py", n_sample=5000,
+              run_preprocess=1, ):
     """
       Configuration of the model is in config_model.py file
 
@@ -386,8 +386,8 @@ def run_train(model_name, path_data, path_output, path_config_model="source/conf
 
 
     log("#### Model Dynamic loading  ######################################################")
-    model_dict_fun = load_function_uri(uri_name= path_config_model + ":" + model_name)    
-    # model_dict_fun = getattr(importlib.import_module("config_model"), model_name)
+    model_dict_fun = load_function_uri(uri_name=path_config_model + "::" + config_model_name)
+    # model_dict_fun = getattr(importlib.import_module("config_model"), config_model_name)
     model_dict     = model_dict_fun(path_model_out)   ### params
 
 
--- titanic_classifier.py
+++ titanic_classifier.py
@@ -1,84 +1,157 @@
-"""%load_ext autoreload
+# pylint: disable=C0321,C0103,E1221,C0301,E1305,E1121,C0302,C0330
+# -*- coding: utf-8 -*-
+"""
+You can put hardcode here, specific to titatinic dataet
+All in one file config
+
+!  python titanic_classifier.py  train
+!  python titanic_classifier.py  check
+!  python titanic_classifier.py  predict
+
+
+
+"""
+"""
+%load_ext autoreload
 %autoreload
 %matplotlib inline
-%config IPCompleter.greedy=True"""
-import warnings,copy
-from source import run_train
-from source import run_inference
-from source import config_model
-from source.models import model_sklearn
+%config IPCompleter.greedy=True
+"""
+import warnings, copy
 warnings.filterwarnings('ignore')
-
-
+import os, sys
+import pandas as pd
 
 ############################################################################
-from allimport import *
 from source import util_feature
 
 
-############################################################################
+
+###### Path ################################################################
 print( os.getcwd())
-root      = os.getcwd()
+root = os.path.abspath(os.getcwd()).replace("\\", "/") + "/"
+print(root)
+
 dir_data  = os.path.abspath( root + "/data/" ) + "/"
 dir_data  = dir_data.replace("\\", "/")
 print(dir_data)
 
 
 
+##### Params#######################################################################
+def titanic_lightgbm(path_model_out) :
+    """
+       titanic
+    """
+    def post_process_fun(y):
+        ### After prediction is done
+        return  y.astype('int')
+
+    def pre_process_fun(y):
+        ### Before the prediction is done
+        return  y.astype('int')
+
+    model_dict = {'model_pars': {'config_model_name': 'LGBMClassifier'    ## Class name for model_sklearn.py
+        , 'model_path': path_model_out
+        , 'model_pars': {'objective': 'binary','learning_rate':0.03,'boosting_type':'gbdt' }  # default
+        , 'post_process_fun': post_process_fun
+        , 'pre_process_pars': {'y_norm_fun' :  None ,
+                               }
+                                 },
+      'compute_pars': { 'metric_list': ['accuracy_score','average_precision_score']
+                      },
+
+      'data_pars': {
+          'cols_input_type' : {
+                     "coly"   :   "Survived"
+                    ,"colid"  :   "PassengerId"
+                    ,"colcat" :   [  "Sex", "Embarked" ]
+                    ,"colnum" :   ["Pclass", "Age","SibSp", "Parch","Fare"]
+                    ,"coltext" :  ["Name","Ticket"]
+                    ,"coldate" :  []
+                    ,"colcross" : [ "Name", "Sex", "Ticket","Embarked","Pclass", "Age","SibSp", "Parch","Fare" ]
+                   },
+
+          # cols['cols_model'] = cols["colnum"] + cols["colcat_bin"]  # + cols[ "colcross_onehot"]
+          'cols_model_group': [ 'colnum', 'colcat_bin']
+         ,'cols_model':       []  # cols['colcat_model'],
+         ,'coly':             []        # cols['coly']
+         ,'filter_pars': { 'ymax' : 2 ,'ymin' : -1 }   ### Filter data
+
+         }}
+    return model_dict
+
+
+
 ############################################################################
-############################################################################
-run_train.run_train('titanic_lightgbm', 'data/input/titanic/train/', '/data/output/a01_lightgbm_classifier/', 
-                	path_config_model="source/config_model.py" , n_sample = -1)
+########## Train ###########################################################
+def train():
+    from source import run_train
+
+    run_train.run_train(config_model_name = 'titanic_lightgbm',
+                        path_data         =  'data/input/titanic/train/',
+                        path_output       =  'data/output/titanic/a01_lightgbm/',
+                        path_config_model =  root + "/titanic_classifier.py" , n_sample = -1)
 
 
-from source.util_feature import load
-from source.models import model_sklearn as modelx
-import sys
-from source import models
-sys.modules['models']=models
-model_tag =  "a01_lightgbm_classifier"
+###################################################################################
+######### Check model #############################################################
+def check():
+    #### Load model
+    from source.util_feature import load
+    from source.models import model_sklearn as modelx
+    import sys
+    from source import models
+    sys.modules['models'] = models
+    model_tag =  "a01_titanic_lightgbm"
+
+
+    dir_model    = dir_data + f"/output/{model_tag}/"
+    modelx.model = load( dir_model + "/model/model.pkl" )
+    stats        = load( dir_model + "/model/info.pkl" )
+    colsX        = load( dir_model + "/model/colsX.pkl"   )
+    coly         = load( dir_model + "/model/coly.pkl"   )
+    print(stats)
+    print(modelx.model.model)
+
+    ### Metrics on test data
+    stats['metrics_test']
+
+    #### Loading training data  #######################################################
+    dfX     = pd.read_csv(dir_model + "/check/dfX.csv")
+    dfy     = dfX[coly]
+    colused = colsX
+
+    dfXtest = pd.read_csv(dir_model + "/check/dfXtest.csv")
+    dfytest = dfXtest[coly]
+    print(dfX.shape,  dfXtest.shape )
+
+
+    #### Feature importance on training data
+    lgb_featimpt_train,_ = util_feature.feature_importance_perm(modelx, dfX[colused], dfy, colused, n_repeats=1,
+                                                                scoring='accuracy' )
+
+    print(lgb_featimpt_train)
+
+    #! python source/run_inference.py  run_predict  --config_model_name  LGBMRegressor  --n_sample 1000   --path_model /data/output/a01_lightgbm_huber/    --path_output /data/output/pred_a01_lightgbm_huber/    --path_data /data/input/train/
 
 
 
-
-############################################################################
-############################################################################
-#### Load model
-dir_model    = dir_data + f"/output/{model_tag}/"
-modelx.model = load( dir_model + "/model/model.pkl" )
-stats        = load( dir_model + "/model/info.pkl" )
-colsX        = load( dir_model + "/model/colsX.pkl"   )
-coly         = load( dir_model + "/model/coly.pkl"   )
-print(stats)
-print(modelx.model.model)
-
-### Metrics on test data
-stats['metrics_test']
-
-#### Loading training data  ###############################################
-dfX     = pd.read_csv(dir_model + "/check/dfX.csv")
-dfy     = dfX[coly]
-colused = colsX
-
-dfXtest = pd.read_csv(dir_model + "/check/dfXtest.csv")
-dfytest = dfXtest[coly]
-
-print(dfX.shape,  dfXtest.shape )
-
-
-#### Feature importance on training data
-lgb_featimpt_train,_ = util_feature.feature_importance_perm(modelx, dfX[colused], dfy, colused, n_repeats=1,  
-	                                                        scoring='accuracy' )
-
-print(lgb_featimpt_train)
-
-#! python source/run_inference.py  run_predict  --model_name  LGBMRegressor  --n_sample 1000   --path_model /data/output/a01_lightgbm_huber/    --path_output /data/output/pred_a01_lightgbm_huber/    --path_data /data/input/train/
+##################################################################################
+####### Inference ################################################################
+def predict():
+    from source import run_inference
+    run_inference.run_predict('LGBMClassifier',
+                              path_model  = '/data/output/titanic/a01_lightgbm/',
+                              path_data   = 'data/input/titanic/test/',
+                              path_output = '/data/output/titanic/pred_a01_titanic_lightgbm/',
+                              n_sample    = -1)
 
 
 
-run_inference.run_predict('LGBMClassifier', '/data/output/a01_lightgbm_classifier/','data/input/titanic/test/', '/data/output/a01_lightgbm_classifier/', -1)
-
-
+if __name__ == "__main__":
+    import fire
+    fire.Fire()
 
 
 
