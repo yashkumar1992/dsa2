7f75e64a23967869a3513865d779664c6a6a5fc1
mozin <mohsinvindhani@gmail.com>
2020-11-27 12:57:19 +0530

    house regression elasticnetcv code
    Former-commit-id: c40fe0eb5ff979fb857a86777de6c90462f2a6a2

--- house_regression.py
+++ house_regression.py
@@ -8,12 +8,12 @@ All in one file config
 !  python house_regression.py  predict
 
 
-# 'pipe_list'  : 
-	'filter', 
-	'label', 
-	'dfnum_bin' 
-	'dfnum_hot'  
-	'dfcat_bin' 
+# 'pipe_list'  :
+	'filter',
+	'label',
+	'dfnum_bin'
+	'dfnum_hot'
+	'dfcat_bin'
 	'dfcat_hot'
 	'dfcross_hot'
 
@@ -26,6 +26,7 @@ warnings.filterwarnings('ignore')
 import os, sys
 import pandas as pd
 import copy
+import pdb
 ############################################################################
 from source import util_feature
 
@@ -37,7 +38,7 @@ config_file  = "house_regression.py"
 data_name    = "house_price"
 
 
-config_name  = 'house_price_lightgbm'
+config_name  = 'house_price_elasticnetcv'
 n_sample     = 1000
 tag_job      = 'aa1'  ## to have a unique tag for the run
 
@@ -95,12 +96,12 @@ def y_norm(y, inverse=True, mode='boxcox'):
 def house_price_elasticnetcv(path_model_out=""):
 
 	global path_config_model, path_model, path_data_train, path_data_test, path_output_pred, n_sample,model_name
-
 	model_name        = 'ElasticNetCV'
+
 	path_config_model = root + f"/{config_file}"
-	path_model        = f'/data/output/{data_name}/a01_{model_name}/'
-	path_data_train   = f'/data/input/{data_name}/train/'
-	path_data_test    = f'/data/input/{data_name}/test/'
+	path_model        = f'data/output/{data_name}/a01_{model_name}/'
+	path_data_train   = f'data/input/{data_name}/train/'
+	path_data_test    = f'data/input/{data_name}/test/'
 	path_output_pred  = f'/data/output/{data_name}/pred_a01_{config_name}/'
 
 
@@ -110,21 +111,16 @@ def house_price_elasticnetcv(path_model_out=""):
 	def pre_process_fun(y):
 		return y_norm(y, inverse=False, mode='boxcox')
 
+	model_dict = {'model_pars': {'config_model_name': 'ElasticNetCV'
+		, 'model_path': path_model_out
+		, 'model_pars': {}  # default ones
+		, 'post_process_fun': post_process_fun
+		, 'pre_process_pars': {'y_norm_fun' : pre_process_fun,
 
-	model_dict = {'model_pars': {
-		  'model_path'       : path_model_out
-
-	    , 'config_model_name': model_name		
-		, 'model_pars'       : {}  # default ones of the model name
-
-		, 'post_process_fun' : post_process_fun
-		, 'pre_process_pars' : {'y_norm_fun' : copy.deepcopy(pre_process_fun),
-
-		   ### Pipeline for data processing.
-		   # 'pipe_list'  : [ 'filter', 'label', 'dfnum_bin', 'dfnum_hot',  'dfcat_bin', 'dfcat_hot', 'dfcross_hot', ]
-		   'pipe_list'  : [ 'filter', 'label',  'dfcat_bin', 'dfcat_hot', ]
-
-		   }
+						### Pipeline for data processing.
+					   # 'pipe_list'  : [ 'filter', 'label', 'dfnum_bin', 'dfnum_hot',  'dfcat_bin', 'dfcat_hot', 'dfcross_hot', ]
+					   'pipe_list' : [ 'filter', 'label',   'dfcat_hot' ]
+													 }
 														 },
 	'compute_pars': { 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',
 									  'explained_variance_score', 'r2_score', 'median_absolute_error']
@@ -137,27 +133,22 @@ def house_price_elasticnetcv(path_model_out=""):
 		    ,"colcat" : [  "MSSubClass", "MSZoning", "Street", "Alley", "LotShape", "LandContour",
 			      "Utilities", "LotConfig", "LandSlope", "Neighborhood", "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "ExterQual", "ExterCond", "Foundation", "BsmtQual", "BsmtCond", "BsmtExposure", "BsmtFinType1", "BsmtFinType2", "Heating", "HeatingQC", "CentralAir", "Electrical", "KitchenQual", "Functional", "FireplaceQu", "GarageType", "GarageFinish", "GarageQual", "GarageCond", "PavedDrive", "PoolQC", "Fence", "MiscFeature", "SaleType", "SaleCondition"]
 
-		    ,"colnum" : ["LotFrontage", "LotArea", "OverallQual", "OverallCond", "MasVnrArea",
-			      "BsmtFinSF1", "BsmtFinSF2", "BsmtUnfSF", "TotalBsmtSF", "1stFlrSF", "2ndFlrSF", "LowQualFinSF", "GrLivArea", "BsmtFullBath", "BsmtHalfBath", "FullBath", "HalfBath", "BedroomAbvGr", "KitchenAbvGr", "TotRmsAbvGrd", "Fireplaces", "GarageCars", "GarageArea", "WoodDeckSF", "OpenPorchSF", "EnclosedPorch", "3SsnPorch", "ScreenPorch", "PoolArea", "MiscVal", "MoSold", "YrSold"]
+		    ,"colnum" : [ "LotArea", "OverallQual", "OverallCond", "MasVnrArea",
+			      "BsmtFinSF1", "BsmtUnfSF", "TotalBsmtSF", "1stFlrSF", "2ndFlrSF", "LowQualFinSF", "GrLivArea", "BsmtFullBath", "BsmtHalfBath", "FullBath", "HalfBath", "BedroomAbvGr", "KitchenAbvGr", "TotRmsAbvGrd", "Fireplaces", "GarageCars", "GarageArea", "WoodDeckSF", "OpenPorchSF", "EnclosedPorch", "3SsnPorch", "ScreenPorch", "PoolArea", "MiscVal", "MoSold", "YrSold"]
 
 		    ,"coltext"  : []
-		    ,"coldate" :  []   # ["YearBuilt", "YearRemodAdd", "GarageYrBlt"]
+		    ,"coldate" : []   # ["YearBuilt", "YearRemodAdd", "GarageYrBlt"]
 		    ,"colcross" : []
 
 		},
 
-			# 'cols_model_group': [ 'colnum_onehot', 'colcat_onehot', 'colcross_onehot' ]
-			'cols_model_group': [ 'colnum', 'colcat_bin' ]
-		 
+		# 'cols_model_group': [ 'colnum_onehot', 'colcat_onehot', 'colcross_onehot' ]
+		'cols_model_group': [ 'colnum', 'colcat_onehot' ]
 
+		 ,'cols_model': []  # cols['colcat_model'],
+		 ,'coly': []        # cols['coly']
 		 ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
-		 					
-
-         ### Keep them emtpy   ########
-		 ,'cols_model': []  
-		 ,'coly':       []       
-	}}	 
-
+							}}
 	return model_dict
 
 
@@ -188,7 +179,7 @@ def house_price_lightgbm(path_model_out="") :
 	model_dict = {'model_pars': {
 		  'model_path'       : path_model_out
 
-	    , 'config_model_name': model_name		
+	    , 'config_model_name': model_name
 		, 'model_pars'       : {}  # default ones of the model name
 
 		, 'post_process_fun' : post_process_fun
@@ -222,15 +213,15 @@ def house_price_lightgbm(path_model_out="") :
 
 		# 'cols_model_group': [ 'colnum_onehot', 'colcat_onehot', 'colcross_onehot' ]
 		'cols_model_group': [ 'colnum', 'colcat_bin' ]
-	 
+
 
 	 ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
-	 					
+
 
      ### Keep them emtpy   ########
-	 ,'cols_model': []  
-	 ,'coly': []       
-	}}	 
+	 ,'cols_model': []
+	 ,'coly': []
+	}}
 
 	return model_dict
 
@@ -249,9 +240,9 @@ globals()[config_name]()
 def data_profile():
    from source.run_feature_profile import run_profile
    run_profile(path_data   = path_data_train,
-               path_output = path_model + "/profile/",  
+               path_output = path_model + "/profile/",
                n_sample    = 5000,
-              ) 
+              )
 
 
 ###################################################################################
