3b34d9a31d6854118339a24503ac1f48a55e12cc
Issam Akouaouch <75189802+akouaouchissam@users.noreply.github.com>
2020-12-06 01:25:16 +0100

    Update airbnb_regression.py

--- airbnb_regression.py
+++ airbnb_regression.py
@@ -309,8 +309,14 @@ path_output_pred  = f'/data/output/{data_name}/pred_a01_{config_name}/'
 ########## Preprocess #############################################################
 def preprocess():
 	from source import run_preprocess
-
+	from text_processing import airbnb
 	run_preprocess.run_preprocess(model_name        =  config_name,
+																path_data         =  path_data_train,
+																path_output       =  path_model,
+																path_config_model =  path_config_model,
+																n_sample          =  n_sample,
+																mode              =  'run_preprocess')
+	airbnb.run_text_preprocess(model_name        =  config_name,
 																path_data         =  path_data_train,
 																path_output       =  path_model,
 																path_config_model =  path_config_model,
@@ -322,12 +328,13 @@ def preprocess():
 def train():
 	from source import run_train
 
+
 	run_train.run_train(config_model_name =  config_name,
 											path_data         =  path_data_train,
 											path_output       =  path_model,
 											path_config_model =  path_config_model , n_sample = n_sample)
 
-
+	
 ###################################################################################
 ######### Check model #############################################################
 def check():
