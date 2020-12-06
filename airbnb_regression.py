2e34ad12535b96f7db14536bd61c2f0d27d9c6da
arita37 <noelkev0@gmail.com>
2020-12-06 21:26:09 +0900

    airbnb_regression.py

--- airbnb_regression.py
+++ airbnb_regression.py
@@ -13,9 +13,6 @@ import warnings, copy
 warnings.filterwarnings('ignore')
 import os, sys
 import pandas as pd
-
-############################################################################
-from source import util_feature
 import copy
 
 ###### Path ################################################################
@@ -26,6 +23,12 @@ print(root)
 dir_data  = os.path.abspath( root + "/data/" ) + "/"
 dir_data  = dir_data.replace("\\", "/")
 print(dir_data)
+
+
+############################################################################
+from source import util_feature
+
+
 
 
 ############################################################################
@@ -42,9 +45,7 @@ n_sample     = 1000
 ###########################################################################
 ####### Pre-processor  ####################################################   
 
-
-
-####### y normalization ###################################################   
+####### y normalization ###################################################
 def y_norm(y, inverse=True, mode='boxcox'):
 	## Normalize the input/output
 	if mode == 'boxcox':
@@ -72,7 +73,7 @@ def y_norm(y, inverse=True, mode='boxcox'):
 			return y
 
 
-##### Custom processor ######################################################
+##### Custom processor ##############################################################
 from data.input.airbnb import airbnb_preprocess
 
 
@@ -82,24 +83,23 @@ from data.input.airbnb import airbnb_preprocess
 ####################################################################################
 ##### Params########################################################################  
 """
-colnum = [  "review_scores_communication", "review_scores_location", "review_scores_rating"         ]
-colcat = [ "cancellation_policy", "host_response_rate", "host_response_time" ]
+colnum  = [  "review_scores_communication", "review_scores_location", "review_scores_rating"   ]
+colcat  = [ "cancellation_policy", "host_response_rate", "host_response_time" ]
 coltext = ["house_rules", "neighborhood_overview", "notes", "street"  ]
 coldate = [  "calendar_last_scraped", "first_review", "host_since" ]
 
-
+usdpricescol = ["price","weekly_price","monthly_price","security_deposit","cleaning_fee","extra_people"]
+    
 
 """
 cols_input_type = {
-     "coly"   : "price"
-    ,"colid"  : "id"
-    ,"colcat" : [  "host_id", "host_location", "host_response_time","host_response_rate","host_is_superhost","host_neighbourhood","host_verifications","host_has_profile_pic","host_identity_verified","street","neighbourhood","neighbourhood_cleansed", "neighbourhood_group_cleansed","city","zipcode", "smart_location","is_location_exact","property_type","room_type", "accommodates","bathrooms","bedrooms", "beds","bed_type","guests_included","calendar_updated", "license","instant_bookable","cancellation_policy","require_guest_profile_picture","require_guest_phone_verification","scrape_id"]
-    ,"colnum" : [ "host_listings_count","latitude", "longitude","square_feet","weekly_price","monthly_price", "security_deposit","cleaning_fee","extra_people", "minimum_nights","maximum_nights","availability_30","availability_60","availability_90","availability_365","number_of_reviews","review_scores_rating","review_scores_accuracy","review_scores_cleanliness","review_scores_checkin","review_scores_communication", "review_scores_location","review_scores_value","calculated_host_listings_count","reviews_per_month"]    
-    ,"coltext" : ["name","summary", "space","description", "neighborhood_overview","notes","transit", "access","interaction", "house_rules","host_name","host_about","amenities"]
+     "coly"     : "price"
+    ,"colid"    : "id"
+    ,"colcat"   : ["host_id", "host_location", "host_response_time","host_response_rate","host_is_superhost","host_neighbourhood","host_verifications","host_has_profile_pic","host_identity_verified","street","neighbourhood","neighbourhood_cleansed", "neighbourhood_group_cleansed","city","zipcode", "smart_location","is_location_exact","property_type","room_type", "accommodates","bathrooms","bedrooms", "beds","bed_type","guests_included","calendar_updated", "license","instant_bookable","cancellation_policy","require_guest_profile_picture","require_guest_phone_verification","scrape_id"]
+    ,"colnum"   : ["host_listings_count","latitude", "longitude","square_feet","weekly_price","monthly_price", "security_deposit","cleaning_fee","extra_people", "minimum_nights","maximum_nights","availability_30","availability_60","availability_90","availability_365","number_of_reviews","review_scores_rating","review_scores_accuracy","review_scores_cleanliness","review_scores_checkin","review_scores_communication", "review_scores_location","review_scores_value","calculated_host_listings_count","reviews_per_month"]
+    ,"coltext"  : ["name","summary", "space","description", "neighborhood_overview","notes","transit", "access","interaction", "house_rules","host_name","host_about","amenities"]
     , "coldate" : ["last_scraped","host_since","first_review","last_review"]
     ,"colcross" : ["name","host_is_superhost","is_location_exact","monthly_price","review_scores_value","review_scores_rating","reviews_per_month"]
-	,"usdpricescol":["price","weekly_price","monthly_price","security_deposit","cleaning_fee","extra_people"]
-    
 }
 
 
@@ -209,8 +209,6 @@ def airbnb_elasticnetcv(path_model_out=""):
 
 
 
-
-
 def airbnb_bayesian_pyro(path_model_out="") :
 	global model_name
 	model_name        = 'model_bayesian_pyro'
@@ -259,15 +257,6 @@ def airbnb_bayesian_pyro(path_model_out="") :
 							 
 
 
-###### Path ################################################################
-print( os.getcwd())
-root = os.path.abspath(os.getcwd()).replace("\\", "/") + "/"
-print(root)
-
-dir_data  = os.path.abspath( root + "/data/" ) + "/"
-dir_data  = dir_data.replace("\\", "/")
-print(dir_data)
-
 
 
 ####################################################################################################
@@ -287,74 +276,37 @@ path_output_pred  = f'/data/output/{data_name}/pred_a01_{config_name}/'
 def preprocess():
 	from source import run_preprocess
 	from text_processing import airbnb
-	run_preprocess.run_preprocess(model_name        =  config_name,
+	from data.input.airbnb import airbnb_preprocess
+
+	run_preprocess.run_preprocess(model_name      =  config_name,
 								path_data         =  path_data_train,
 								path_output       =  path_model,
 								path_config_model =  path_config_model,
 								n_sample          =  n_sample,
 								mode              =  'run_preprocess')
 
-	airbnb.run_text_preprocess(model_name        =  config_name,
+	airbnb_preprocess.run_text_preprocess(model_name        =  config_name,
 								path_data         =  path_data_train,
 								path_output       =  path_model,
 								path_config_model =  path_config_model,
 								n_sample          =  n_sample,
 								mode              =  'run_preprocess')
+
 
 ############################################################################
 ########## Train ###########################################################
 def train():
 	from source import run_train
-
-
 	run_train.run_train(config_model_name =  config_name,
-											path_data         =  path_data_train,
-											path_output       =  path_model,
-											path_config_model =  path_config_model , n_sample = n_sample)
+						path_data         =  path_data_train,
+						path_output       =  path_model,
+						path_config_model =  path_config_model , n_sample = n_sample)
 
 	
 ###################################################################################
 ######### Check model #############################################################
 def check():
-	try :
-		#### Load model
-		from source.util_feature import load
-		from source.models import model_sklearn as modelx
-		import sys
-		from source import models
-		sys.modules['models'] = models
-
-		dir_model    = path_model
-		modelx.model = load( dir_model + "/model/model.pkl" )
-		stats        = load( dir_model + "/model/info.pkl" )
-		colsX        = load( dir_model + "/model/colsX.pkl"   )
-		coly         = load( dir_model + "/model/coly.pkl"   )
-		print(stats)
-		print(modelx.model.model)
-
-		### Metrics on test data
-		stats['metrics_test']
-
-		#### Loading training data  #######################################################
-		ddfX     = pd.read_csv(dir_model + "/check/dfX.csv")  #to load csv
-		#dfX = pd.read_parquet(dir_model + "/check/dfX.parquet")    #to load parquet
-		dfy     = dfX[coly]
-		colused = colsX
-
-		dfXtest = pd.read_csv(dir_model + "/check/dfXtest.csv")    #to load csv
-		#dfXtest = pd.read_parquet(dir_model + "/check/dfXtest.parquet"    #to load parquet
-		dfytest = dfXtest[coly]
-		print(dfX.shape,  dfXtest.shape )
-
-
-		#### Feature importance on training data
-		lgb_featimpt_train,_ = util_feature.feature_importance_perm(modelx, dfX[colused], dfy, colused, n_repeats=1,
-																																scoring='neg_root_mean_squared_error' )
-
-		print(lgb_featimpt_train)
-	except :
-			pass
-	#! python source/run_inference.py  run_predict  --config_model_name  LGBMRegressor  --n_sample 1000   --path_model /data/output/a01_lightgbm_huber/    --path_output /data/output/pred_a01_lightgbm_huber/    --path_data /data/input/train/
+   pass
 
 
 
@@ -389,22 +341,5 @@ python  airbnb_regression.py  run_all
 if __name__ == "__main__":
 		import fire
 		fire.Fire()
-		
-"""
 
 
-import template_run
-template_run.config_name       = config_name
-template_run.path_config_model = path_config_model
-template_run.path_model        = path_model
-template_run.path_data_train   = path_data_train
-template_run.path_data_test    = path_data_test
-template_run.path_output_pred  = path_output_pred
-template_run.n_sample          = n_sample
-template_run.model_name        = model_name
-
-print( template_run.config_name )
-train                          = template_run.train
-predict                        = template_run.predict
-run_all                        = template_run.run_all
-"""
