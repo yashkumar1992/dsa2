fb4e150c322482f504546a32865b869d3f592fed
arita37 <noelkev0@gmail.com>
2020-12-08 16:06:50 +0900

    airbnb_regression.py

--- airbnb_regression.py
+++ airbnb_regression.py
@@ -3,9 +3,12 @@
 """
 You can put hardcode here, specific to titatinic dataet
 All in one file config
-!  python airbnb_regression.py  train    > zlog/log-multi_classifier.txt 2>&1
-!  python airbnb_regression.py  check    > zlog/log-multi_classifier.txt 2>&1
-!  python airbnb_regression.py  predict  > zlog/log-multi_classifier.txt 2>&1
+
+  python airbnb_regression.py  preprocess
+
+  python airbnb_regression.py  train    > zlog/log-airbnb.txt 2>&1
+  python airbnb_regression.py  check    > zlog/log-airbnb.txt 2>&1
+  python airbnb_regression.py  predict  > zlog/log-airbnb.txt 2>&1
 
 
 
@@ -117,29 +120,35 @@ def airbnb_lightgbm(path_model_out="") :
     """
 
     """
-    global model_name
+    global path_config_model, path_model, path_data_train, path_data_test, path_output_pred, n_sample,model_name
+
+    config_name       = 'airbnb_lightgbm'
     model_name        = 'LGBMRegressor'
+    n_sample          = 1000
 
     def post_process_fun(y):
         return y_norm(y, inverse=True, mode='boxcox')
 
+
     def pre_process_fun(y):
         return y_norm(y, inverse=False, mode='boxcox')
 
+
     model_dict = {'model_pars': {'config_model_name': 'LGBMRegressor'
         ,'model_path': path_model_out
-        ,'model_pars': {'objective': 'huber', }  # default
+        ,'model_pars': {'objective': 'huber',
+
+                       }  # lightgbm one
         ,'post_process_fun': post_process_fun
         ,'pre_process_pars': {'y_norm_fun' :  copy.deepcopy(pre_process_fun) ,
 
-
-
                     ### Pipeline for data processing.
                     'pipe_list'  : ['filter', 'label',   'dfcat_bin', 'dftext'   ]
+
         }
     },
 
-    'compute_pars': { 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',
+    'compute_pars': { 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',   #### sklearm names
                                       'explained_variance_score', 'r2_score', 'median_absolute_error']
                     },
 
@@ -147,98 +156,37 @@ def airbnb_lightgbm(path_model_out="") :
           'cols_input_type' : cols_input_type_2
 
 
-         ,'cols_model_group': [ 'colnum', 'colcat_bin']
+         ,'cols_model_group': [  'colnum'
+                                ,'colcat_bin'
+                                ,'coldate'
+                                ,'coltext'
+                              ]
 
 
          ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
 
          }}
+
+
+    ################################################################################################
+    ##### Filling Global parameters    #############################################################
+    path_config_model = root + f"/{config_file}"
+    path_model        = f'data/output/{data_name}/{model_name}/'
+    path_data_train   = f'data/input/{data_name}/train/'
+    path_data_test    = f'data/input/{data_name}/test/'
+    path_output_pred  = f'/data/output/{data_name}/pred_a01_{config_name}/'
+
+    model_dict[ 'global_pars'] = {}
+    global_pars = [ 'config_name', 'model_name', 'path_config_model', 'path_model', 'path_data_train',
+                    'path_data_test', 'path_output_pred', 'n_sample'
+            ]
+    for t in global_pars:
+      model_dict['global_pars'][t] = globals()[t]
+
+
     return model_dict
  
 
-
-
-def airbnb_elasticnetcv(path_model_out=""):
-    global model_name
-    model_name        = 'ElasticNetCV'
-
-    def post_process_fun(y):
-        return y_norm(y, inverse=True, mode='boxcox')
-
-    def pre_process_fun(y):
-        return y_norm(y, inverse=False, mode='boxcox')
-
-    model_dict = {'model_pars': {'config_model_name': 'ElasticNetCV'
-        , 'model_path': path_model_out
-        , 'model_pars': {}  # default ones
-        , 'post_process_fun': post_process_fun
-        , 'pre_process_pars': {'y_norm_fun' : pre_process_fun,
-
-                        ### Pipeline for data processing.
-                       'pipe_list'  : [ 'filter', 'label', 'dfnum_bin', 'dfnum_hot',  'dfcat_bin', 'dfcat_hot', 'dfcross_hot',
-                                        'dfdate', 'dftext'
-
-                        ]
-                                                     }
-                                                         },
-    'compute_pars': { 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',
-                                      'explained_variance_score', 'r2_score', 'median_absolute_error']
-                                    },
-    'data_pars': {
-            'cols_input_type' : cols_input_type_1,
-
-            'cols_model_group': [ 'colnum_onehot', 'colcat_onehot', 'colcross_onehot' ]
-
-
-         ,'cols_model': []  # cols['colcat_model'],
-         ,'coly': []        # cols['coly']
-         ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
-                            }}
-    return model_dict
-
-
-
-
-
-def airbnb_bayesian_pyro(path_model_out="") :
-    global model_name
-    model_name        = 'model_bayesian_pyro'
-    def post_process_fun(y):
-        return y_norm(y, inverse=True, mode='boxcox')
-
-    def pre_process_fun(y):
-        return y_norm(y, inverse=False, mode='boxcox')
-
-    model_dict = {'model_pars': {'config_model_name': 'model_bayesian_pyro'
-        , 'model_path': path_model_out
-        , 'model_pars': {'input_width': 112, }  # default
-        , 'post_process_fun': post_process_fun
-
-        , 'pre_process_pars': {'y_norm_fun' :  copy.deepcopy(pre_process_fun) ,
-
-                        ### Pipeline for data processing.
-                       'pipe_list'  : [ 'filter', 'label', 'dfnum_bin', 'dfnum_hot',  'dfcat_bin', 'dfcat_hot', 'dfcross_hot', ]
-          }
-       },
-
-    'compute_pars': {'compute_pars': {'n_iter': 1200, 'learning_rate': 0.01}
-                                 , 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',
-                                                    'explained_variance_score', 'r2_score', 'median_absolute_error']
-                                 , 'max_size': 1000000
-                                 , 'num_samples': 300
-     },
-    'data_pars':  {
-	   'cols_input_type' : cols_input_type_1
-
-
-
-	  ,'cols_model_group': [ 'colnum_onehot', 'colcat_onehot' ]
-
-
-
-	  ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
-    }}
-    return model_dict
 
 
 
@@ -318,5 +266,97 @@ python  airbnb_regression.py  run_all
 if __name__ == "__main__":
         import fire
         fire.Fire()
+
+
+
+
+
+
+
+
+
+
+
+
+
+def airbnb_elasticnetcv(path_model_out=""):
+    global model_name
+    model_name        = 'ElasticNetCV'
+
+    def post_process_fun(y):
+        return y_norm(y, inverse=True, mode='boxcox')
+
+    def pre_process_fun(y):
+        return y_norm(y, inverse=False, mode='boxcox')
+
+    model_dict = {'model_pars': {'config_model_name': 'ElasticNetCV'
+        , 'model_path': path_model_out
+        , 'model_pars': {}  # default ones
+        , 'post_process_fun': post_process_fun
+        , 'pre_process_pars': {'y_norm_fun' : pre_process_fun,
+
+                        ### Pipeline for data processing.
+                       'pipe_list'  : [ 'filter', 'label', 'dfnum_bin', 'dfnum_hot',  'dfcat_bin', 'dfcat_hot', 'dfcross_hot',
+                                        'dfdate', 'dftext'
+
+                        ]
+                                                     }
+                                                         },
+    'compute_pars': { 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',
+                                      'explained_variance_score', 'r2_score', 'median_absolute_error']
+                                    },
+    'data_pars': {
+            'cols_input_type' : cols_input_type_1,
+
+            'cols_model_group': [ 'colnum_onehot', 'colcat_onehot', 'colcross_onehot' ]
+
+
+         ,'cols_model': []  # cols['colcat_model'],
+         ,'coly': []        # cols['coly']
+         ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
+                            }}
+    return model_dict
+
+
+
+def airbnb_bayesian_pyro(path_model_out="") :
+    global model_name
+    model_name        = 'model_bayesian_pyro'
+    def post_process_fun(y):
+        return y_norm(y, inverse=True, mode='boxcox')
+
+    def pre_process_fun(y):
+        return y_norm(y, inverse=False, mode='boxcox')
+
+    model_dict = {'model_pars': {'config_model_name': 'model_bayesian_pyro'
+        , 'model_path': path_model_out
+        , 'model_pars': {'input_width': 112, }  # default
+        , 'post_process_fun': post_process_fun
+
+        , 'pre_process_pars': {'y_norm_fun' :  copy.deepcopy(pre_process_fun) ,
+
+                        ### Pipeline for data processing.
+                       'pipe_list'  : [ 'filter', 'label', 'dfnum_bin', 'dfnum_hot',  'dfcat_bin', 'dfcat_hot', 'dfcross_hot', ]
+          }
+       },
+
+    'compute_pars': {'compute_pars': {'n_iter': 1200, 'learning_rate': 0.01}
+                                 , 'metric_list': ['root_mean_squared_error', 'mean_absolute_error',
+                                                    'explained_variance_score', 'r2_score', 'median_absolute_error']
+                                 , 'max_size': 1000000
+                                 , 'num_samples': 300
+     },
+    'data_pars':  {
+	   'cols_input_type' : cols_input_type_1
+
+
+
+	  ,'cols_model_group': [ 'colnum_onehot', 'colcat_onehot' ]
+
+
+
+	  ,'filter_pars': { 'ymax' : 100000.0 ,'ymin' : 0.0 }   ### Filter data
+    }}
+    return model_dict
 
 
