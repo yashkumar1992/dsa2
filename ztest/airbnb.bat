b6125f2c693d5b8efc658fcfd5c6b0bb53955e10
arita37 <noelkev0@gmail.com>
2020-12-25 16:53:36 +0900

    ztest/airbnb.bat
    Former-commit-id: a6d5f800120787209fc9b9012cfe6c621647ffa7

--- ztest/airbnb.bat
+++ ztest/airbnb.bat
@@ -14,5 +14,6 @@ del  zlog/log_airbnb_train.txt  >nul 2>&1
 del  zlog/log_airbnb_predict.txt >nul 2>&1
 
 
-  python airbnb_regression.py   preprocess    > zlog/log_airbnb_prepro.txt 2>&1
+  # python airbnb_regression.py   preprocess    > zlog/log_airbnb_prepro.txt 2>&1
   python airbnb_regression.py   train    > zlog/log_airbnb_train.txt 2>&1
+  python airbnb_regression.py   predict    > zlog/log_airbnb_predict.txt 2>&1
