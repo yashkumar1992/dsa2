9e4387218784974a07bc1b275bdeb873fed1d8de
arita37 <noelkev0@gmail.com>
2020-12-28 19:51:59 +0900

    optuna_classifier.py
    Former-commit-id: e2f52396e916c1c118c371a971e6761d3e5e858a

--- optuna_classifier.py
+++ optuna_classifier.py
@@ -109,68 +109,69 @@ def titanic_lightgbm(path_model_out=""):
         return int(y)
 
     model_dict = {'model_pars': {
-        ### LightGBM API model   #######################################
-        'model_class': model_class
-        , 'model_pars': {'objective': 'binary',
-                         'n_estimators': 50,
-                         'learning_rate': 0.001,
-                         'boosting_type': 'gbdt',  ### Model hyperparameters
-                         'early_stopping_rounds': 5
-                         }
+    ### LightGBM API model   #######################################
+    'model_class': model_class
+    , 'model_pars': {'objective': 'binary',
+                     'n_estimators': 50,
+                     'learning_rate': 0.001,
+                     'boosting_type': 'gbdt',  ### Model hyperparameters
+                     'early_stopping_rounds': 5
+                     }
 
-        ### After prediction  ##########################################
-        , 'post_process_fun': post_process_fun
 
-        ### Before training  ##########################################
-        , 'pre_process_pars': {'y_norm_fun': pre_process_fun,
+    ### After prediction  ##########################################
+    , 'post_process_fun': post_process_fun
 
-                               ### Pipeline for data processing ##############################
-                               'pipe_list': [
-                                   {'uri': 'source/preprocessors.py::pd_coly', 'pars': {}, 'cols_family': 'coly',
-                                    'cols_out': 'coly', 'type': 'coly'},
-                                   {'uri': 'source/preprocessors.py::pd_colnum_bin', 'pars': {},
-                                    'cols_family': 'colnum', 'cols_out': 'colnum_bin', 'type': ''},
-                                   {'uri': 'source/preprocessors.py::pd_colnum_binto_onehot', 'pars': {},
-                                    'cols_family': 'colnum_bin', 'cols_out': 'colnum_onehot', 'type': ''},
-                                   {'uri': 'source/preprocessors.py::pd_colcat_bin', 'pars': {},
-                                    'cols_family': 'colcat', 'cols_out': 'colcat_bin', 'type': ''},
-                                   {'uri': 'source/preprocessors.py::pd_colcat_to_onehot', 'pars': {},
-                                    'cols_family': 'colcat_bin', 'cols_out': 'colcat_onehot', 'type': ''},
-                                   {'uri': 'source/preprocessors.py::pd_colcross', 'pars': {},
-                                    'cols_family': 'colcross', 'cols_out': 'colcross_pair_onehot', 'type': 'cross'}
-                               ],
-                               }
+    ### Before training  ##########################################
+    , 'pre_process_pars': {'y_norm_fun': pre_process_fun,
+
+       ### Pipeline for data processing ##############################
+       'pipe_list': [
+           {'uri': 'source/preprocessors.py::pd_coly', 'pars': {}, 'cols_family': 'coly',
+            'cols_out': 'coly', 'type': 'coly'},
+           {'uri': 'source/preprocessors.py::pd_colnum_bin', 'pars': {},
+            'cols_family': 'colnum', 'cols_out': 'colnum_bin', 'type': ''},
+           {'uri': 'source/preprocessors.py::pd_colnum_binto_onehot', 'pars': {},
+            'cols_family': 'colnum_bin', 'cols_out': 'colnum_onehot', 'type': ''},
+           {'uri': 'source/preprocessors.py::pd_colcat_bin', 'pars': {},
+            'cols_family': 'colcat', 'cols_out': 'colcat_bin', 'type': ''},
+           {'uri': 'source/preprocessors.py::pd_colcat_to_onehot', 'pars': {},
+            'cols_family': 'colcat_bin', 'cols_out': 'colcat_onehot', 'type': ''},
+           {'uri': 'source/preprocessors.py::pd_colcross', 'pars': {},
+            'cols_family': 'colcross', 'cols_out': 'colcross_pair_onehot', 'type': 'cross'}
+       ],
+       }
     },
 
-        'compute_pars': {'metric_list': ['accuracy_score', 'average_precision_score'],
-                         'optuna_params': {
-                             "early_stopping_rounds": 100,
-                             "objective": "binary",
-                             "metric": "binary_logloss",
-                             "verbosity": -1,
-                             "boosting_type": "gbdt",
-                         }
-                         },
+    'compute_pars': {'metric_list': ['accuracy_score', 'average_precision_score'],
+                     'optuna_params': {
+                         "early_stopping_rounds": 10,
+                         "objective": "binary",
+                         "metric": "binary_logloss",
+                         "verbosity": -1,
+                         "boosting_type": "gbdt",
+                     }
+                     },
 
-        'data_pars': {'n_sample': n_sample,
-                      'cols_input_type': cols_input_type_1,
-                      ### family of columns for MODEL  #########################################################
-                      #  "colnum", "colnum_bin", "colnum_onehot", "colnum_binmap",  #### Colnum columns
-                      #  "colcat", "colcat_bin", "colcat_onehot", "colcat_bin_map",  #### colcat columns
-                      #  'colcross_single_onehot_select', "colcross_pair_onehot",  'colcross_pair',  #### colcross columns
-                      #  'coldate',
-                      #  'coltext',
-                      'cols_model_group': ['colnum_bin',
-                                           'colcat_bin',
-                                           # 'coltext',
-                                           # 'coldate',
-                                           # 'colcross_pair'
-                                           ]
+    'data_pars': {'n_sample': n_sample,
+                  'cols_input_type': cols_input_type_1,
+                  ### family of columns for MODEL  #########################################################
+                  #  "colnum", "colnum_bin", "colnum_onehot", "colnum_binmap",  #### Colnum columns
+                  #  "colcat", "colcat_bin", "colcat_onehot", "colcat_bin_map",  #### colcat columns
+                  #  'colcross_single_onehot_select', "colcross_pair_onehot",  'colcross_pair',  #### colcross columns
+                  #  'coldate',
+                  #  'coltext',
+                  'cols_model_group': ['colnum_bin',
+                                       'colcat_bin',
+                                       # 'coltext',
+                                       # 'coldate',
+                                       # 'colcross_pair'
+                                       ]
 
-                      ### Filter data rows   ##################################################################
-            , 'filter_pars': {'ymax': 2, 'ymin': -1}
+                  ### Filter data rows   ##################################################################
+        , 'filter_pars': {'ymax': 2, 'ymin': -1}
 
-                      }
+                  }
     }
 
     ##### Filling Global parameters    ############################################################
