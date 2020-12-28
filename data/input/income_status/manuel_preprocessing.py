b1aea930112458afadfdcf4b24ad92bd348c671b
arita37 <noelkev0@gmail.com>
2020-12-29 00:08:02 +0900

    data/input/income_status/manuel_preprocessing.py

--- data/input/income_status/manuel_preprocessing.py
+++ data/input/income_status/manuel_preprocessing.py
@@ -24,10 +24,104 @@ df_test = df_test.rename(columns = {'25':'age' , ' Private':'workclass', ' 22680
 
 
 
-def pd_income_processor(df, col, pars):
+
+
+
+
+
+
+def pd_cleanup(df, col, pars):
+  df.drop(['education'], axis=1, inplace = True)
+  df.replace(" ?",np.NaN,inplace=True)
+
+  # Converting the value of the target column to a 0-1
+  df.status = [ 0 if each == " <=50K" else 1 for each in df.status]
+  df["id"]=[i+1 for i in range(len(df))]
+  return df
+
+
+
+
+def pd_normalize_quantile(df, col=['age', 'final_weight', 'capital-gain', 'capital-loss', 'hours-per-week'], pars={}):
   """
      Processor for DSA@
   """
+  df = df[col]
+
+  num_col = col
+  # num_col=['age', 'final_weight', 'capital-gain', 'capital-loss', 'hours-per-week']
+  sparse_col= pars.get('colsparse', ['capital-gain', 'capital-loss'] )
+
+  # Find IQR and implement to numericals and sparse columns seperately
+  Q1  = df.quantile(0.25)
+  Q3  = df.quantile(0.75)
+  IQR = Q3 - Q1
+
+  for col in num_col:
+    if col in sparse_col:
+      nonsparse_data = pd.DataFrame(df[df[col] !=df[col].mode()[0]][col])
+      if nonsparse_data[col].quantile(0.25) < df[col].mode()[0]: #Unexpected case
+        lower_bound_sparse = nonsparse_data[col].quantile(0.25)
+      else:
+        lower_bound_sparse = df[col].mode()[0]
+
+      if nonsparse_data[col].quantile(0.75) < df[col].mode()[0]: #Unexpected case
+        upper_bound_sparse = df[col].mode()[0]
+      else:
+        upper_bound_sparse = nonsparse_data[col].quantile(0.75)
+
+      number_of_outliers = len(df[(df[col] < lower_bound_sparse) | (df[col] > upper_bound_sparse)][col])
+
+      if number_of_outliers > 0:
+
+        df.loc[df[col] < lower_bound_sparse,col] = lower_bound_sparse*0.75 #--> MAIN DF CHANGED
+
+        df.loc[df[col] > upper_bound_sparse,col] = upper_bound_sparse*1.25 # --> MAIN DF CHANGED
+    else:
+      lower_bound = df[col].quantile(0.25) - 1.5*IQR[col]
+      upper_bound = df[col].quantile(0.75) + 1.5*IQR[col]
+
+      df[col] = np.where(df[col] > upper_bound, 1.25*upper_bound, df[col])
+      df[col] = np.where(df[col] < lower_bound, 0.75*lower_bound, df[col])
+
+
+  colnew   = [ t + "_norm" for t in df.columns ]
+  pars_new = {'lower_bound' : lower_bound, 'upper_bound': upper_bound}
+  dfnew    = df
+
+  encoder_model = None
+  ###################################################################################
+  if 'path_features_store' in pars and 'path_pipeline_export' in pars:
+     # pass
+     #save_features(df, 'dfcat_encoder', pars['path_features_store'])
+     # save(colnew,    pars['path_pipeline_export']  + "/colnum_encoder_model.pkl" )
+     save(pars_new,  pars['path_pipeline_export']   + "/colnum_quantile_norm_new.pkl" )
+
+
+  col_pars = {}
+  col_pars['model']    = encoder_model
+  col_pars['pars']     = pars_new
+  col_pars['cols_new'] = {
+   'colnum_normalize_quantile' :  colnew  ### list
+  }
+
+  #dfnew    = df.drop(["status"],axis=1)
+  return dfnew, col_pars
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def pd_income_processor(df, col, pars):
+  # Processor for DSA@
   #df = df.rename(columns = {'39':'age' , ' State-gov':'workclass', ' 77516':'final_weight', ' Bachelors':'education', ' 13':'education-num', ' Never-married':'marital-status', ' Adm-clerical':'occupation', ' Not-in-family':'relationship', ' White':'race', ' Male':'sex', ' 2174':'capital-gain', ' 0':'capital-loss', ' 40':'hours-per-week', ' United-States':'native-country', ' <=50K':'status'})
 
 
@@ -104,14 +198,20 @@ def pd_income_processor(df, col, pars):
 
     #dfnew    = df.drop(["status"],axis=1)
     return dfnew, col_pars
-
+"""
 
 
 """
 	Saving files to csv and also zip format
 """
 
-df         = pd_income_processor(df, list(df.columns), pars={} )
+df = pd_cleanup(df, col=None, pars=None)
+
+df = pd_normalize_quantile(df,
+                           col=['age', 'final_weight', 'capital-gain', 'capital-loss', 'hours-per-week'], pars={})
+
+
+#df         = pd_income_processor(df, list(df.columns), pars={} )
 feature_tr = df[0].drop(["status"],axis=1)
 target_tr  = df[0][["status","id"]]
 feature_tr.to_csv( "train/features.csv", index=False)
@@ -126,6 +226,12 @@ target_tr.to_csv('train/target.zip', index=False,compression=target)
 
 
 
+#####
+df_test = pd_cleanup(df_test, col=None, pars=None)
+
+df_test = pd_normalize_quantile(df_test,
+                  col=['age', 'final_weight', 'capital-gain', 'capital-loss', 'hours-per-week'], pars={})
+
 df_test      = pd_income_processor(df_test, list(df_test.columns), pars={} )
 feature_test = df_test[0].drop(["status"],axis=1)
 target_test  = df_test[0][["status","id"]]
@@ -138,75 +244,12 @@ target_test.to_csv('test/target.zip', index=False, compression=target)
 
 
 
-"""
-	No need to use this function
-
-"""
-   
-
-def pd_income_processor_old(df, col, pars):
-  """
-  """
-  df = df.rename(columns = {'39':'age' , ' State-gov':'workclass', ' 77516':'final_weight', ' Bachelors':'education', ' 13':'education-num', ' Never-married':'marital-status', ' Adm-clerical':'occupation', ' Not-in-family':'relationship', ' White':'race', ' Male':'sex', ' 2174':'capital-gain', ' 0':'capital-loss', ' 40':'hours-per-week', ' United-States':'native-country', ' <=50K':'status'})
-  df_test = df_test.rename(columns = {'25':'age' , ' Private':'workclass', ' 226802':'final_weight', ' 11th':'education', ' 7':'education-num', ' Never-married':'marital-status', ' Machine-op-inspct':'occupation', ' Own-child':'relationship', ' Black':'race', ' Male':'sex', ' 0':'capital-gain', ' 0.1':'capital-loss', ' 40':'hours-per-week', ' United-States':'native-country', ' <=50K.':'status'})
 
 
-  df.drop(['education'], axis=1, inplace = True)
-  df_test.drop(['education'], axis=1, inplace = True)
-
-  # The replacing of "?" with "nan" on the dataset
-  df.replace(" ?",np.NaN,inplace=True)
-  df_test.replace(" ?",np.NaN,inplace=True)
-
-  # Converting the value of the target column to a 0-1
-  df.status = [ 0 if each == " <=50K" else 1 for each in df.status]
-  df_test.status = [ 0 if each == " <=50K" else 1 for each in df_test.status]
 
 
-  df["id"]=[i+1 for i in range(len(df))]
-  df_test["id"]=[i+1 for i in range(len(df_test))]
 
 
-  num_col=['age', 'final_weight', 'capital-gain', 'capital-loss', 'hours-per-week']
-  cat_col=['occupation','workclass','native-country','education-num','marital-status','relationship','race','sex','status']
-  sparse_col=['capital-gain', 'capital-loss']
 
-  # Find IQR and implement to numericals and sparse columns seperately
-  Q1 = df.quantile(0.25)
-  Q3 = df.quantile(0.75)
-  IQR = Q3 - Q1
 
-  for col in num_col:
-    if col in sparse_col:
-      nonsparse_data = pd.DataFrame(df[df[col] !=df[col].mode()[0]][col])
-      if nonsparse_data[col].quantile(0.25) < df[col].mode()[0]: #Unexpected case
-        lower_bound_sparse = nonsparse_data[col].quantile(0.25)
-      else:
-        lower_bound_sparse = df[col].mode()[0]
 
-      if nonsparse_data[col].quantile(0.75) < df[col].mode()[0]: #Unexpected case
-        upper_bound_sparse = df[col].mode()[0]
-      else:
-        upper_bound_sparse = nonsparse_data[col].quantile(0.75)
-
-      number_of_outliers = len(df[(df[col] < lower_bound_sparse) | (df[col] > upper_bound_sparse)][col])
-
-      if number_of_outliers > 0:
-
-        df.loc[df[col] < lower_bound_sparse,col] = lower_bound_sparse*0.75 #--> MAIN DF CHANGED
-
-        df.loc[df[col] > upper_bound_sparse,col] = upper_bound_sparse*1.25 # --> MAIN DF CHANGED
-    else:
-      lower_bound = df[col].quantile(0.25) - 1.5*IQR[col]
-      upper_bound = df[col].quantile(0.75) + 1.5*IQR[col]
-
-      df[col] = np.where(df[col] > upper_bound, 1.25*upper_bound, df[col])
-      df[col] = np.where(df[col] < lower_bound, 0.75*lower_bound, df[col])
-
-      df_test[col] = np.where(df_test[col] > upper_bound, 1.25*upper_bound, df_test[col])
-      df_test[col] = np.where(df_test[col] < lower_bound, 0.75*lower_bound, df_test[col])
-
-    return dfnew, col_pars
-	
-	
-	
