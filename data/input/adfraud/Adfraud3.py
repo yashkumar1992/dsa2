6d91b38376857432429f00146f2e134150c15192
MohamedAMINE.BenSlama <medaminebenslama@gmail.com>
2020-12-30 01:44:53 +0100

    mitigate class imbalance

--- data/input/adfraud/Adfraud3.py
+++ data/input/adfraud/Adfraud3.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import random
 import copy
-
+import imblearn
+import time
 random.seed(100)
 
 df = pd.read_csv("raw/train_sample.csv")
@@ -83,8 +84,8 @@ def pd_colall_preprocess(df, col=None, pars=None):
         "count").astype("uint8")
 
     df['n_ip_app_os_day_hh'] = \
-    df[['ip', 'app', 'os', 'day', 'hour', 'channel']].groupby(by=['ip', 'app', 'os', 'day', 'hour'])[
-        ['channel']].transform("count").astype("uint8")
+        df[['ip', 'app', 'os', 'day', 'hour', 'channel']].groupby(by=['ip', 'app', 'os', 'day', 'hour'])[
+            ['channel']].transform("count").astype("uint8")
     print('Computing the number of channels associated with ')
     df['ip_app_count'] = df[['ip', 'app', 'channel']].groupby(by=['ip', 'app'])[['channel']].transform("count").astype(
         "uint8")
@@ -173,8 +174,8 @@ def pd_colall_preprocess(df, col=None, pars=None):
     df['n_ip_app_day_hh'] = df[['ip', 'app', 'day', 'hour', 'channel']].groupby(by=['ip', 'app', 'day', 'hour'])[
         ['channel']].transform("count").astype("uint8")
     df['n_ip_app_os_day_hh'] = \
-    df[['ip', 'app', 'os', 'day', 'hour', 'channel']].groupby(by=['ip', 'app', 'os', 'day', 'hour'])[
-        ['channel']].transform("count").astype("uint8")
+        df[['ip', 'app', 'os', 'day', 'hour', 'channel']].groupby(by=['ip', 'app', 'os', 'day', 'hour'])[
+            ['channel']].transform("count").astype("uint8")
     df['n_ip_app_dev_os'] = df[['ip', 'app', 'device', 'os', 'channel']].groupby(by=['ip', 'app', 'device', 'os'])[
         ['channel']].transform("count").astype("uint8")
     df['n_ip_dev_os'] = df[['ip', 'device', 'os', 'channel']].groupby(by=['ip', 'device', 'os'])[['channel']].transform(
@@ -297,3 +298,95 @@ from sklearn import metrics
 ypred = bst.predict(test_X_all)
 score = metrics.roc_auc_score(test_y, ypred)
 print(f"Test score: {score}") #Best test score: Test score: 0.9802690018944903 Other executions : Test score: 0.9790603799985851 Test score: 0.954113637971559
+
+
+### Since the data is highly imbalanced we use lightgbm scale_pos_weight
+
+def lgb_modelfit_nocv(params, dtrain, dvalid, objective='binary', metrics='auc',
+                      feval=None, early_stopping_rounds=20, num_boost_round=3000,verbose_eval=10):
+    lgb_params = {
+        'boosting_type': 'gbdt',
+        'objective': objective,
+        'metric': metrics,
+        'learning_rate': 0.01,
+        # 'is_unbalance': 'true',  #because training data is unbalance (replaced with scale_pos_weight)
+        'num_leaves': 31,  # we should let it be smaller than 2^(max_depth)
+        'max_depth': -1,  # -1 means no limit
+        'min_child_samples': 20,  # Minimum number of data need in a child(min_data_in_leaf)
+        'max_bin': 255,  # Number of bucketed bin for feature values
+        'subsample': 0.6,  # Subsample ratio of the training instance.
+        'subsample_freq': 0,  # frequence of subsample, <=0 means no enable
+        'colsample_bytree': 0.3,  # Subsample ratio of columns when constructing each tree.
+        'min_child_weight': 5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+        'subsample_for_bin': 200000,  # Number of samples for constructing bin
+        'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+        'reg_alpha': 0,  # L1 regularization term on weights
+        'reg_lambda': 0,  # L2 regularization term on weights
+        'nthread': 4,
+        'verbose': 0,
+    }
+
+    lgb_params.update(params)
+
+    print("preparing validation datasets")
+
+    xgtrain = dtrain
+    xgvalid = dvalid
+
+    evals_results = {}
+
+    bst1 = lgb.train(lgb_params,
+                     xgtrain,
+                     valid_sets=[xgtrain, xgvalid],
+                     valid_names=['train', 'valid'],
+                     evals_result=evals_results,
+                     num_boost_round=num_boost_round,
+                     early_stopping_rounds=early_stopping_rounds,
+                     verbose_eval=10,
+                     feval=feval)
+
+    n_estimators = bst1.best_iteration
+    print("\nModel Report")
+    print("n_estimators : ", n_estimators)
+    print(metrics + ":", evals_results['valid'][metrics][n_estimators - 1])
+
+    return bst1
+
+print("Training...")
+start_time = time.time()
+
+params = {
+    'learning_rate': 0.15,
+    # 'is_unbalance': 'true', # replaced with scale_pos_weight argument
+    'num_leaves': 7,  # 2^max_depth - 1
+    'max_depth': 3,  # -1 means no limit
+    'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+    'max_bin': 100,  # Number of bucketed bin for feature values
+    'subsample': 0.7,  # Subsample ratio of the training instance.
+    'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+    'colsample_bytree': 0.9,  # Subsample ratio of columns when constructing each tree.
+    'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+    'scale_pos_weight': 99  # because training data is extremely unbalanced
+}
+bst = lgb_modelfit_nocv(params,
+                        dtrain,
+                        dvalid,
+                        objective='binary',
+                        metrics='auc',
+                        early_stopping_rounds=30,
+                        verbose_eval=True,
+                        num_boost_round=500,
+                        )
+
+print('[{}]: model training time'.format(time.time() - start_time))
+from sklearn import metrics
+
+ypred = bst.predict(test_X)
+score = metrics.roc_auc_score(test_y, ypred)
+print(f"Test score: {score}")
+
+# Model Report
+# n_estimators :  10
+# auc: 0.936902301739492
+# [0.5453190803527832]: model training time
+# Test score: 0.9526652569353279
