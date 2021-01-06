dd08ec448ebf9f3ea1c932f6a68b070b17bb7284
arita37 <noelkev0@gmail.com>
2021-01-07 00:48:55 +0900

    tseries gluon

--- auto_feat.py
+++ auto_feat.py
@@ -16,37 +16,42 @@ print( os.getcwd())
 root = os.path.abspath(os.getcwd()).replace("\\", "/") + "/"
 print(root)
 
-dir_data  = os.path.abspath( root + "/data/" ) + "/"
-dir_data  = dir_data.replace("\\", "/")
+#dir_data  = os.path.abspath( root + "/data/" ) + "/"
+#dir_data  = dir_data.replace("\\", "/")
+import zlocal
+dir_data = zlocal.dir_data
+dir_data = dir_data.replace("\\", "/")
 print(dir_data)
+
 
 def os_get_function_name():
     import sys
     return sys._getframe(1).f_code.co_name
 
+
 def global_pars_update(model_dict,  data_name, config_name):
     m                      = {}
-    m['config_path']       = root + f"/{config_file}"
+    m['config_path']       = dir_data + f"/{config_file}"
     m['config_name']       = config_name
 
     ##### run_Preoprocess ONLY
-    m['path_data_preprocess'] = root + f'/data/input/{data_name}/train/'
+    m['path_data_preprocess'] = dir_data + f'/input/{data_name}/train/'
 
     ##### run_Train  ONLY
-    m['path_data_train']   = root + f'/data/input/{data_name}/train/'
-    m['path_data_test']    = root + f'/data/input/{data_name}/test/'
-    #m['path_data_val']    = root + f'/data/input/{data_name}/test/'
-    m['path_train_output']    = root + f'/data/output/{data_name}/{config_name}/'
-    m['path_train_model']     = root + f'/data/output/{data_name}/{config_name}/model/'
-    m['path_features_store']  = root + f'/data/output/{data_name}/{config_name}/features_store/'
-    m['path_pipeline']        = root + f'/data/output/{data_name}/{config_name}/pipeline/'
+    m['path_data_train']   = dir_data + f'/input/{data_name}/train/'
+    m['path_data_test']    = dir_data + f'/input/{data_name}/test/'
+    #m['path_data_val']    = dir_data + f'/input/{data_name}/test/'
+    m['path_train_output']    = dir_data + f'/output/{data_name}/{config_name}/'
+    m['path_train_model']     = dir_data + f'/output/{data_name}/{config_name}/model/'
+    m['path_features_store']  = dir_data + f'/output/{data_name}/{config_name}/features_store/'
+    m['path_pipeline']        = dir_data + f'/output/{data_name}/{config_name}/pipeline/'
 
 
     ##### Prediction
-    m['path_pred_data']    = root + f'/data/input/{data_name}/test/'
-    m['path_pred_pipeline']= root + f'/data/output/{data_name}/{config_name}/pipeline/'
-    m['path_pred_model']   = root + f'/data/output/{data_name}/{config_name}/model/'
-    m['path_pred_output']  = root + f'/data/output/{data_name}/pred_{config_name}/'
+    m['path_pred_data']    = dir_data + f'/input/{data_name}/test/'
+    m['path_pred_pipeline']= dir_data + f'/output/{data_name}/{config_name}/pipeline/'
+    m['path_pred_model']   = dir_data + f'/output/{data_name}/{config_name}/model/'
+    m['path_pred_output']  = dir_data + f'/output/{data_name}/pred_{config_name}/'
 
     #####  Generic
     m['n_sample']             = model_dict['data_pars'].get('n_sample', 5000)
