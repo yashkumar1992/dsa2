24dcfc7c349415f8de447bc5fe90700ba7006ea2
Kev. Noel <arita37@users.noreply.github.com>
2021-01-16 17:46:54 +0900

    Update run_full.sh

--- ztest/run_full.sh
+++ ztest/run_full.sh
@@ -11,22 +11,22 @@ ls .
 # python classifier_multi.py  train    --nsample 10000   ;
 
 
-python regress_salary.py  train   --nsample 1000
-python regress_salary.py  predict  --nsample 1000
+python example/regress_salary.py  train   --nsample 1000
+python example/regress_salary.py  predict  --nsample 1000
 
-python regress_cardif.py  train   --nsample 1000
-
-
-python regress_airbnb.py  train   --nsample 20000
-python regress_airbnb.py  predict  --nsample 5000
+python example/regress_cardif.py  train   --nsample 1000
 
 
+python example/regress_airbnb.py  train   --nsample 20000
+python example/regress_airbnb.py  predict  --nsample 5000
 
 
 
 
-python classifier_income.py  train    --nsample 1000   ;
-python classifier_income.py  predict  --nsample 1000   ;
+
+
+python example/classifier_income.py  train    --nsample 1000   ;
+python example/classifier_income.py  predict  --nsample 1000   ;
 
 
 
