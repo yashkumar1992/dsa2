2354fd14a62a23af26ce4276cd17912537911db5
Kev. Noel <arita37@users.noreply.github.com>
2021-01-23 23:37:06 +0900

    Update model_gefs.py

--- source/models/model_gefs.py
+++ source/models/model_gefs.py
@@ -3,12 +3,16 @@
 """
 """
 import os
-
 import pandas as pd
 import numpy as np
 import sklearn
 
-from gefs import RandomForest
+try :
+  from gefs import RandomForest
+except :
+  os.system( " python -m pip install git+https://github.com/arita37/GeFs/GeFs.git@aa32d657013b7cacf62aaad912a9b88110cee5d1  -y ")
+  from gefs import RandomForest
+
 
 ####################################################################################################
 VERBOSE = True
@@ -38,64 +42,15 @@ class Model(object):
             self.model = None
         else:
             self.n_estimators = model_pars.get('n_estimators', 100)
-            self.ncat = model_pars.get('ncat', None)  # Number of categories of each variable This is an ndarray
+            self.ncat         = model_pars.get('ncat', None)  # Number of categories of each variable This is an ndarray
             if self.ncat is None:
                 self.model = None  # In order to create an instance of the model we need to calculate the ncat mentioned above on our dataset
+                log('ncat is not define')
             else:
                 self.model = RandomForest(n_estimators=self.n_estimators, ncat=self.ncat)
             if VERBOSE: log(None, self.model)
 
-
-def is_continuous(df):
-    """
-        Returns true if df was sampled from a continuous variables, and false
-        Otherwise.
-        Parameters
-        ----------
-        df: numpy
-            One dimensional array containing the values of one variable.
-    """
-    observed = df[~np.isnan(df)]  # not consider missing values for this.
-    rules = [np.min(observed) < 0,
-             np.sum((observed) != np.round(observed)) > 0,
-             len(np.unique(observed)) > min(30, len(observed) / 3)]
-    if any(rules):
-        return True
-    else:
-        return False
-
-
-def pd_colcat_get_catcount(df, classcol=None, continuous_ids=[]):
-    """
-        Learns the number of categories in each variable and standardizes the df.
-        Parameters
-        ----------
-        df: numpy n x m
-            Numpy array comprising n realisations (instances) of m variables.
-        classcol: int
-            The column index of the class variables (if any).
-        continuous_ids: list of ints
-            List containing the indices of known continuous variables. Useful for
-            discrete df like age, which is better modeled as continuous.
-        Returns
-        -------
-        ncat: numpy m
-            The number of categories of each variable. One if the variable is
-            continuous.
-    """
-    df = df.copy()
-    ncat = np.ones(df.shape[1])
-    if not classcol:
-        classcol = df.shape[1] - 1
-    for i in range(df.shape[1]):
-        if i != classcol and (i in continuous_ids or is_continuous(df[:, i])):
-            continue
-        else:
-            df[:, i] = df[:, i].astype(int)
-            ncat[i] = max(df[:, i]) + 1
-    return ncat
-
-
+                
 def fit(data_pars=None, compute_pars=None, out_pars=None, **kw):
     """
     """
@@ -105,11 +60,11 @@ def fit(data_pars=None, compute_pars=None, out_pars=None, **kw):
     if VERBOSE: log(Xtrain.shape, model.model)
 
     if model.ncat is None:
-        #!IMPORTANT This indicates that the preprocessing pipeline was not adapted to GEFS! and we need to calculate ncat
-        cont_cols = data_pars['cols_input_type'].get("colcat")  # is this correct?
+        log("#!IMPORTANT This indicates that the preprocessing pipeline was not adapted to GEFS! and we need to calculate ncat")
+        cont_cols  = data_pars['cols_input_type'].get("colnum")  #  continous, float column is this correct?
         temp_train = pd.concat([Xtrain, ytrain], axis=1)
-        temp_test = pd.concat([Xtest, ytest], axis=1)
-        df = pd.concat([temp_train, temp_test], ignore_index=True, sort=False)
+        temp_test  = pd.concat([Xtest, ytest],   axis=1)
+        df         = pd.concat([temp_train, temp_test], ignore_index=True, sort=False)
         model.ncat = pd_colcat_get_catcount(df.values, classcol=-1,
                                             continuous_ids=[df.columns.get_loc(c) for c in cont_cols])
 
@@ -125,7 +80,7 @@ def eval(data_pars=None, compute_pars=None, out_pars=None, **kw):
     """
     global model, session
     data_pars['train'] = True
-    Xval, yval = get_dataset(data_pars, task_type="eval")
+    Xval, yval        = get_dataset(data_pars, task_type="eval")
     # ypred      = model.model.predict(Xval)
     ypred, ypred_prob = predict(Xval, data_pars, compute_pars, out_pars)
 
@@ -153,14 +108,13 @@ def predict(Xpred=None, data_pars={}, compute_pars={}, out_pars={}, **kw):
 
     if Xpred is None:
         data_pars['train'] = False
-        Xpred = get_dataset(data_pars, task_type="predict")
+        Xpred              = get_dataset(data_pars, task_type="predict")
 
-    ypred, y_prob = model.model.classify(Xpred, classcol=Xpred.shape[1], return_prob=True)
-    y_prob = np.max(y_prob, axis=1)
-
-    ypred_proba = None  ### No proba
-    if compute_pars.get("probability", False):
-         ypred_proba = y_prob
+    ypred, y_prob = model.model.classify(Xpred, classcol = Xpred.shape[1], return_prob=True)
+    ypred         = post_process_fun(y
+    y_prob        = np.max(y_prob, axis=1)
+    
+    ypred_proba = y_prob  if compute_pars.get("probability", False) else None
 
     return ypred, ypred_proba
 
@@ -271,10 +225,10 @@ def get_params_sklearn(deep=False):
 def get_params(param_pars={}, **kw):
     import json
     # from jsoncomment import JsonComment ; json = JsonComment()
-    pp = param_pars
-    choice = pp['choice']
+    pp          = param_pars
+    choice      = pp['choice']
     config_mode = pp['config_mode']
-    data_path = pp['data_path']
+    data_path   = pp['data_path']
 
     if choice == "json":
         cf = json.load(open(data_path, mode='r'))
@@ -284,42 +238,57 @@ def get_params(param_pars={}, **kw):
     else:
         raise Exception(f"Not support choice {choice} yet")
 
-def test_model_gefs():
+        
+        
+####################################################################################################        
+############ Custom Code ############################################################################
+def is_continuous(v_array):
+    """
+        Returns true if df was sampled from a continuous variables, and false
+    """
+    observed = v_array[~np.isnan(v_array)]  # not consider missing values for this.
+    rules    = [np.min(observed) < 0,
+                np.sum((observed) != np.round(observed)) > 0,
+                len(np.unique(observed)) > min(30, len(observed) / 3)]
+    if any(rules):
+        return True
+    else:
+        return False
+
+
+def pd_colcat_get_catcount(df, classcol=None, continuous_ids=[]):
+    """
+        Learns the number of categories in each variable and standardizes the df.
+        Parameters
+        ----------
+        df: numpy n x m  Numpy array comprising n realisations (instances) of m variables.
+        classcol: int   The column index of the class variables (if any).
+        continuous_ids: list of ints
+            List containing the indices of known continuous variables. Useful for
+            discrete df like age, which is better modeled as continuous.
+        Returns
+        -------
+        ncat: numpy m The number of categories of each variable. One if the variable is continuous.
+    """
+    df   = df.copy()
+    ncat = np.ones(df.shape[1])
+    if not classcol:
+        classcol = df.shape[1] - 1
+    for i in range(df.shape[1]):
+        if i != classcol and (i in continuous_ids or is_continuous(df[:, i])):
+            continue
+        else:
+            df[:, i] = df[:, i].astype(int)
+            ncat[i] = max(df[:, i]) + 1
+    return ncat
+
+                                     
+def test_model():
     # Auxiliary functions
-    def learncats(data, classcol=None, continuous_ids=[]):
-        """
-            Learns the number of categories in each variable and standardizes the data.
-            Parameters
-            ----------
-            data: numpy n x m
-                Numpy array comprising n realisations (instances) of m variables.
-            classcol: int
-                The column index of the class variables (if any).
-            continuous_ids: list of ints
-                List containing the indices of known continuous variables. Useful for
-                discrete data like age, which is better modeled as continuous.
-            Returns
-            -------
-            ncat: numpy m
-                The number of categories of each variable. One if the variable is
-                continuous.
-        """
-        data = data.copy()
-        ncat = np.ones(data.shape[1])
-        if not classcol:
-            classcol = data.shape[1] - 1
-        for i in range(data.shape[1]):
-            if i != classcol and (i in continuous_ids or is_continuous(data[:, i])):
-                continue
-            else:
-                data[:, i] = data[:, i].astype(int)
-                ncat[i] = max(data[:, i]) + 1
-        return ncat
 
     def get_stats(data, ncat=None):
         """
-            Compute univariate statistics for continuous variables.
-            Parameters
+            Compute univariate statistics for continuous variables. Parameters
             ----------
             data: numpy n x m
                 Numpy array comprising n realisations (instances) of m variables.
@@ -338,7 +307,7 @@ def test_model_gefs():
         maxv = np.ones(data.shape[1])
         minv = np.zeros(data.shape[1])
         mean = np.zeros(data.shape[1])
-        std = np.zeros(data.shape[1])
+        std  = np.zeros(data.shape[1])
         if ncat is not None:
             for i in range(data.shape[1]):
                 if ncat[i] == 1:
@@ -354,26 +323,16 @@ def test_model_gefs():
                     maxv[i] = np.max(data[:, i])
                     minv[i] = np.min(data[:, i])
                     mean[i] = np.mean(data[:, i])
-                    std[i] = np.std(data[:, i])
+                    std[i]  = np.std(data[:, i])
                     assert maxv[i] != minv[i], 'Cannot have constant continuous variable in the data'
                     data[:, i] = (data[:, i] - minv[i]) / (maxv[i] - minv[i])
         return data, maxv, minv, mean, std
 
+                                     
     def standardize_data(data, mean, std):
         """
             Standardizes the data given the mean and standard deviations values of
             each variable.
-            Parameters
-            ----------
-            data: numpy n x m
-                Numpy array comprising n realisations (instances) of m variables.
-            mean, std: numpy m
-                The mean and standard deviation of the variable. Zero and one, resp.
-                if the variable is categorical.
-            Returns
-            -------
-            data: numpy n x m
-                The standardized data.
         """
         data = data.copy()
         for v in range(data.shape[1]):
@@ -383,25 +342,24 @@ def test_model_gefs():
                 data[:, v] = np.clip(data[:, v], -6, 6)
         return data
 
+                                     
     def train_test(data, ncat, train_ratio=0.7, prep='std'):
         assert train_ratio >= 0
         assert train_ratio <= 1
-        shuffle = np.random.choice(range(data.shape[0]), data.shape[0], replace=False)
+        shuffle    = np.random.choice(range(data.shape[0]), data.shape[0], replace=False)
         data_train = data[shuffle[:int(train_ratio * data.shape[0])], :]
-        data_test = data[shuffle[int(train_ratio * data.shape[0]):], :]
-        if prep == 'norm':
-            data_train, maxv, minv, _, _, = get_stats(data_train, ncat)
-            X_train, y_train = data_train[:, :-1], data_train[:, -1]
-            return X_train, y_train, data_train, data_test, maxv, minv
-        elif prep == 'std':
+        data_test  = data[shuffle[int(train_ratio * data.shape[0]):], :]
+                                                    
+        if prep == 'std':
             _, maxv, minv, mean, std = get_stats(data_train, ncat)
-            data_train = standardize_data(data_train, mean, std)
-            X_train, y_train = data_train[:, :-1], data_train[:, -1]
+            data_train               = standardize_data(data_train, mean, std)
+            X_train, y_train         = data_train[:, :-1], data_train[:, -1]
             return X_train, y_train, data_train, data_test, mean, std
 
+                                     
     # Load toy dataset
-    df_white = pd.read_csv('../../data/input/wine-quality/raw/winequality-white.csv', sep=';').values
-    ncat_white = learncats(df_white, classcol=-1)
+    df_white   = pd.read_csv('../../data/input/wine-quality/raw/winequality-white.csv', sep=';').values
+    ncat_white = pd_colcat_get_catcount(df_white, classcol=-1)
     ncat_white[-1] = 2
 
     X_train_white, y_train_white, data_train_white, data_test_white, mean_white, std_white = train_test(df_white,
@@ -419,4 +377,51 @@ def test_model_gefs():
 
     print('ok')
 
-test_model_gefs()
+                                     
+if __name__ == "__main__":
+    import fire
+    fire.Fire()
+                                     
+"""
+python model_gef.py test_model
+
+
+"""
+                                     
+                                     
+                                     
+                                     
+"""
+
+    def learncats(data, classcol=None, continuous_ids=[]):
+  
+            Learns the number of categories in each variable and standardizes the data.
+            Parameters
+            ----------
+            data: numpy n x m
+                Numpy array comprising n realisations (instances) of m variables.
+            classcol: int
+                The column index of the class variables (if any).
+            continuous_ids: list of ints
+                List containing the indices of known continuous variables. Useful for
+                discrete data like age, which is better modeled as continuous.
+            Returns
+            -------
+            ncat: numpy m
+                The number of categories of each variable. One if the variable is
+                continuous.
+      
+        data = data.copy()
+        ncat = np.ones(data.shape[1])
+        if not classcol:
+            classcol = data.shape[1] - 1
+        for i in range(data.shape[1]):
+            if i != classcol and (i in continuous_ids or is_continuous(data[:, i])):
+                continue
+            else:
+                data[:, i] = data[:, i].astype(int)
+                ncat[i] = max(data[:, i]) + 1
+        return ncat
+
+
+"""
